47a73f66d3d337e1d367d1718f508de505358d1d
chrylt <chris.kobalt@tum.de>
2025-03-16 18:32:26 +0100

    modified utils

--- basalt/src/utils.cpp
+++ basalt/src/utils.cpp
@@ -30,9 +30,9 @@ namespace basalt {
         }
 
         void copyBuffer(Device& device, const CommandPool& commandPool, const VkQueue queue,
-                        const VkBuffer srcBuffer, const VkBuffer dstBuffer, const VkDeviceSize size)
+            const VkBuffer srcBuffer, const VkBuffer dstBuffer, const VkDeviceSize size)
         {
-	        const VkCommandBuffer commandBuffer = commandPool.beginSingleTimeCommands();
+            const VkCommandBuffer commandBuffer = commandPool.beginSingleTimeCommands();
 
             VkBufferCopy copyRegion;
             copyRegion.srcOffset = 0;
@@ -44,10 +44,15 @@ namespace basalt {
             commandPool.endSingleTimeCommands(commandBuffer, queue);
         }
 
-        void transitionImageLayout(Device& device, const CommandPool& commandPool, const VkQueue& queue,
-            const VkImage image, VkFormat format, const VkImageLayout oldLayout, const VkImageLayout newLayout)
+        void transitionImageLayout(Device& device,
+            const CommandPool& commandPool,
+            const VkQueue& queue,
+            VkImage image,
+            VkFormat format,
+            VkImageLayout oldLayout,
+            VkImageLayout newLayout)
         {
-	        const VkCommandBuffer commandBuffer = commandPool.beginSingleTimeCommands();
+            VkCommandBuffer commandBuffer = commandPool.beginSingleTimeCommands();
 
             VkImageMemoryBarrier barrier{};
             barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
@@ -57,26 +62,55 @@ namespace basalt {
             barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
             barrier.image = image;
             barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            barrier.subresourceRange.baseMipLevel = 0;
             barrier.subresourceRange.levelCount = 1;
+            barrier.subresourceRange.baseArrayLayer = 0;
             barrier.subresourceRange.layerCount = 1;
 
-            VkPipelineStageFlags sourceStage;
-            VkPipelineStageFlags destinationStage;
+            VkPipelineStageFlags sourceStage = 0;
+            VkPipelineStageFlags destinationStage = 0;
 
-            if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_GENERAL) {
+            if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_GENERAL)
+            {
+                // UNDEFINED -> GENERAL
                 barrier.srcAccessMask = 0;
-                barrier.dstAccessMask = VK_ACCESS_SHADER_WRITE_BIT | VK_ACCESS_SHADER_READ_BIT;
+                barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT | VK_ACCESS_SHADER_WRITE_BIT;
                 sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
                 destinationStage = VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT | VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
             }
-            // Add other transitions if needed
-            else {
+            else if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
+            {
+                // UNDEFINED -> TRANSFER_DST_OPTIMAL
+                barrier.srcAccessMask = 0;
+                barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+                sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+                destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+            }
+            else if (oldLayout == VK_IMAGE_LAYOUT_GENERAL && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
+            {
+                // GENERAL -> TRANSFER_DST_OPTIMAL
+                barrier.srcAccessMask = VK_ACCESS_SHADER_READ_BIT | VK_ACCESS_SHADER_WRITE_BIT;
+                barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+                sourceStage = VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT | VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+                destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+            }
+            else if (oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_GENERAL)
+            {
+                // TRANSFER_DST_OPTIMAL -> GENERAL
+                barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+                barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT | VK_ACCESS_SHADER_WRITE_BIT;
+                sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+                destinationStage = VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT | VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+            }
+            else
+            {
                 throw std::invalid_argument("Unsupported layout transition!");
             }
 
             vkCmdPipelineBarrier(
                 commandBuffer,
-                sourceStage, destinationStage,
+                sourceStage,
+                destinationStage,
                 0,
                 0, nullptr,
                 0, nullptr,
